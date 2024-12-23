94eb81e743cd17507a0ad7a5b1d0d36637a2307e
Roman Manzhula <roman1985al@ukr.net>
2024-12-23 23:21:22 +0200

    Update HelloServlet.java
    In Ukrainian

--- PA_First_Servlet/src/main/java/com/example/pa_first_servlet/HelloServlet.java
+++ PA_First_Servlet/src/main/java/com/example/pa_first_servlet/HelloServlet.java
@@ -8,18 +8,18 @@ import java.io.IOException;
 import java.io.PrintWriter;
 
 public class HelloServlet extends HttpServlet {
-    static final String TEMPLATE = "<html>" +  //в виде константы добавляем шаблон HTML
-            "<head><title>com.example.pa_first_servlet</title></head>" + //идет HTML-документ, далее %s-определенный
-            "<body><h1>Hello, %s</h1></body></html>"; // модификатор от String.format() для конкатенации строки
+    static final String TEMPLATE = "<html>" +  //як константа додаємо HTML-шаблон
+            "<head><title>com.example.pa_first_servlet</title></head>" + //HTML-документ, далі %s - змінна,
+            "<body><h1>Привіт, %s</h1></body></html>"; //модифікатор від String.format() для конкатенації рядка
+    
     @Override
-    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws IOException { //переопределяем метод
-        //doPost чтобы наши данные не было видно в корне
-        String name = req.getParameter("name"); //создаем переменную для обращения к request вызываем getParameter
-        // и по названию параметра в эту переменную получаем значение в виде строки
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws IOException { //перевизначаємо метод
+        //doPost, щоб наші дані не були видні в URL
+        String name = req.getParameter("name"); //створюємо змінну для доступу до параметра запиту
+        //і отримуємо значення за назвою параметра
 
-        resp.setContentType("text/html"); //для response устанавливаем тип контента - текст/html
-        PrintWriter printWriter = resp.getWriter(); //пишем данные (в нашем случае текст) в переменную
-        printWriter.println(String.format(TEMPLATE, name)); //и выводим клиенту на экран
-
+        resp.setContentType("text/html"); //для відповіді встановлюємо тип контенту - text/html
+        PrintWriter printWriter = resp.getWriter(); //створюємо PrintWriter для запису даних (в нашому випадку тексту)
+        printWriter.println(String.format(TEMPLATE, name)); //і виводимо клієнту на екран
     }
 }
